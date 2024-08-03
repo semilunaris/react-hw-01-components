11b09799a7d1c1943b837d9daa5fad386d788cc1
semilunaris <vlad.karpych@gmail.com>
2024-08-03 14:07:03 +0300

    2task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Profile } from 'Сomponents/Profile'
+import { Profile } from 'Сomponents/Profiles/Profile'
+import { Statistics } from 'Сomponents/Statistics/Statisics'
 import user from '../src/user.json'
+import data from '../src/data.json'
 
 
-export const App =() => {
-return <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>}
+export const App = () => {
+  return (
+    <div>
+      <div><Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      /> </div>
+      <div><Statistics  stats={data} />
+      </div>
+    </div>
+  );
+};
